fcdf1884ee0a950a4f4a606c3eeb7afebb261f64
surikadt <gnidanol@gmail.com>
2023-06-27 14:32:42 +0300

    Added custom constructor for Task, in which isComplete is set to false by default

--- src/main/java/org/toDoList/domain/Task.java
+++ src/main/java/org/toDoList/domain/Task.java
@@ -3,6 +3,9 @@ package org.toDoList.domain;
 import java.time.LocalDate;
 
 public record Task(boolean isComplete, String name, String notes, LocalDate date, Priority priority){
+    public Task(String name, String notes, LocalDate date, Priority priority) {
+        this(false, name, notes, date, priority);
+    }
 
     public enum Priority {
         HIGH("high"), MEDIUM("medium"), LOW("low");
