93ff97fe0958aae47bde25ff93b161e0ad674fe9
surikadt <gnidanol@gmail.com>
2023-06-27 19:57:34 +0300

    Created all necessary dependencies in Main.java and started infinite loop Deleted unnecessary deprecated Main.java class

---
+++ src/main/java/org/toDoList/ui/Main.java
@@ -0,0 +1,30 @@
+package org.toDoList.ui;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.toDoList.domain.Task;
+import org.toDoList.domain.ToDo;
+import org.toDoList.domain.source.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter()
+                ).
+                create();
+        JsonHandler gsonHandler = new GsonHandler(gson);
+        FileDataSource jsonDataSource = new JsonDataSource(gsonHandler);
+        List<Task> tasks = jsonDataSource.readFile();
+        ToDo todo = new ToDo(tasks);
+        ToDoUI toDoUI = new ToDoConsole(scanner, todo);
+        Executor executor = new Executor(jsonDataSource, toDoUI, scanner);
+
+        executor.run();
+    }
+}
